959bc06d19b9105146fc561a70b5171d812f05cd
Michele Cheow <ldnallen@gmail.com>
2024-07-03 14:21:30 -0700

    updated default Welcome component

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,23 +1,19 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+      <Title className={classes.title} ta="center" mt={50}>
+        <Text
+          inherit
+          variant="gradient"
+          component="span"
+          gradient={{ from: '#9fccfa', to: '#696eff' }}
+        >
+          New York Times Article Search
         </Text>
       </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit page.tsx file.
-      </Text>
     </>
   );
 }
