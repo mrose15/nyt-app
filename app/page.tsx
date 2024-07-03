03f0e792ca819cd1948638b5051d2b2622f62b1e
Michele Cheow <ldnallen@gmail.com>
2024-07-03 15:12:44 -0700

    removed Welcome from home page and added to Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Box } from '@mantine/core';
-import { Welcome } from '../components/Welcome/Welcome';
 import Header from '@/components/Header/Header';
 import ArticleList from '../components/Articles/ArticleList.client';
 
@@ -7,9 +5,6 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <Box mb="xxl">
-        <Welcome />
-      </Box>
       <ArticleList query="aviation" />
     </>
   );
