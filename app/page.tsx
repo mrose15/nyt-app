07570a65f79897b0aa39a534e8e2d08406aa1d77
Michele Cheow <ldnallen@gmail.com>
2024-07-04 11:27:41 -0700

    removed search from header and added it to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
+'use client';
+
+import React, { useState } from 'react';
 import Header from '@/components/Header/Header';
 import ArticleList from '../components/Articles/ArticleList.client';
+import SearchComponent from '../components/Search/Search';
 
-export default function HomePage() {
+const HomePage = () => {
+  const [query, setQuery] = useState('aviation');
+  const [noResults, setNoResults] = useState(false); // Step 1: New state to track no results
+
+  const handleSearch = (newQuery: string) => {
+    setQuery(newQuery);
+    setNoResults(false); // Reset no results state on new search
+  };
+
+  // Step 2: Update to handle no results from `ArticleList`
+  const handleNoResults = (isEmpty: boolean) => {
+    setNoResults(isEmpty);
+  };
+
   return (
     <>
       <Header />
-      <ArticleList query="aviation" />
+      <SearchComponent onSearch={handleSearch} />
+      <ArticleList query={query} onNoResults={handleNoResults} />
+      {noResults && (
+        <div>No results found for &quot;{query}&quot;. Please try another search.</div>
+      )}{' '}
     </>
   );
-}
+};
+
+export default HomePage;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,18 +1,16 @@
 import { Group, Container } from '@mantine/core';
 import { Welcome } from '../Welcome/Welcome';
 import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
-import SearchComponent from '../Search/Search';
 
-export default function Header() {
-  return (
-    <Container mb={50} mt={50}>
-      <Group justify="flex-end" mb={20}>
-        <ColorSchemeToggle />
-      </Group>
-      <Group justify="space-between">
-        <Welcome />
-        <SearchComponent />
-      </Group>
-    </Container>
-  );
-}
+const Header = () => (
+  <Container mb={50} mt={50}>
+    <Group justify="flex-end" mb={20}>
+      <ColorSchemeToggle />
+    </Group>
+    <Group justify="space-between">
+      <Welcome />
+    </Group>
+  </Container>
+);
+
+export default Header;
