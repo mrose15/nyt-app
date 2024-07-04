4e745fb297f65599ffc6fd0ac1a469401363f052
Michele Cheow <ldnallen@gmail.com>
2024-07-03 17:22:20 -0700

    more welcome style edits

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -4,12 +4,12 @@ import classes from './Welcome.module.css';
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center">
+      <Title className={classes.title} ta="left">
         <Text
           inherit
           variant="gradient"
           component="span"
-          gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+          gradient={{ from: '#A77CE8', to: '#627af7', deg: 90 }}
         >
           New York Times Article Search
         </Text>
