e3f3894fd1d4569d82af3cbe6baee556fb406789
Michele Cheow <ldnallen@gmail.com>
2024-07-04 13:21:39 -0700

    changed to arrow function

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,22 +8,22 @@ export const metadata = {
   description: 'I am using Mantine with Next.js!',
 };
 
-export default function RootLayout({ children }: { children: any }) {
-  return (
-    <html lang="en">
-      <head>
-        <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-      </head>
-      <body>
-        <MantineProvider theme={theme}>
-          <Container size="xxl">{children}</Container>
-        </MantineProvider>
-      </body>
-    </html>
-  );
-}
+const RootLayout = ({ children }: { children: any }) => (
+  <html lang="en">
+    <head>
+      <ColorSchemeScript />
+      <link rel="shortcut icon" href="/favicon.svg" />
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+      />
+    </head>
+    <body>
+      <MantineProvider theme={theme}>
+        <Container size="xxl">{children}</Container>
+      </MantineProvider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
